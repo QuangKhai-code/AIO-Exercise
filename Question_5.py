748fdfdba04986ff7b75a421b1cbf28771916be9
Khải Bùi <quangkhai.saigon@gmail.com>
2024-06-04 00:43:25 +0700

    Fix Question_5 remove abs in difference

--- Question_5.py
+++ Question_5.py
@@ -1,7 +1,7 @@
 def md_nre_single_sample(y, y_hat, n, p):
     root_y = y ** (1/n)
     root_y_hat = y_hat ** (1/n)
-    difference = abs(root_y - root_y_hat)
+    difference = (root_y - root_y_hat)
     result = difference ** p
     return result
 
